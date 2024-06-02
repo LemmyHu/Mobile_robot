34104906dc101984a343e7dfeeafef6984b8d1c5
LemmyHu <lemmyhu@gmail.com>
2024-06-02 17:40:41 +0200

    Remove build, install and log

--- build/joystick_listener/rosidl_cmake/rosidl_cmake-extras.cmake
+++
@@ -1,4 +0,0 @@
-# generated from rosidl_cmake/cmake/rosidl_cmake-extras.cmake.in
-
-set(joystick_listener_IDL_FILES "msg/JoystickCommand.idl")
-set(joystick_listener_INTERFACE_FILES "msg/JoystickCommand.msg")
